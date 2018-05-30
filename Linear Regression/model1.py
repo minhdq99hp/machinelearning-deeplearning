3b10b99b0c178e60bfcf62638f869e06f0e9b2b5
minhdq99hp <minhdq99hp@gmail.com>
2018-05-31 01:15:00 +0700

    working on Linear Regression

--- Linear Regression/model1.py
+++ Linear Regression/model1.py
@@ -2,10 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # INPUT DATA
+# Getting data form data?.csv file
+import csv
+
+data_file = open('data2.csv')
+data_file_reader = csv.reader(data_file)
+raw_data = list(data_file_reader)
+
+dataset = []
+for data_points in raw_data:
+	dataset.append([float(data_points[0]), float(data_points[1])])
+
+
+
 # height 
-X = np.array([[147, 150, 153, 158, 163, 165, 168, 170, 173, 175, 178, 180, 183]]).T
+X = np.array([[data_points[0] for data_points in dataset]]).T
 # weight
-y = np.array([[ 49, 50, 51,  54, 58, 59, 60, 62, 63, 64, 66, 67, 68]]).T
+y = np.array([[data_points[1] for data_points in dataset]]).T
 
 # Building Xbar 
 one = np.ones((X.shape[0], 1))
@@ -20,13 +33,13 @@ print('w = ', w)
 # Preparing the fitting line 
 w_0 = w[0][0]
 w_1 = w[1][0]
-x0 = np.linspace(145, 185, 2)
+x0 = np.linspace(min([data_points[0] for data_points in dataset]), max([data_points[0] for data_points in dataset]), 2)
 y0 = w_0 + w_1*x0
 
 # Drawing the fitting line 
 plt.plot(X.T, y.T, 'ro')     # data 
 plt.plot(x0, y0)               # the fitting line
-plt.axis([140, 190, 45, 75])
+
 plt.xlabel('Height (cm)')
 plt.ylabel('Weight (kg)')
 plt.show()
